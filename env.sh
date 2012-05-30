915c7f8e5e29d0322490ffb7e704e55418328868
Phil Jones <p.g.jones@qmul.ac.uk>
2012-05-30 18:40:40 +0100

    Nicer update script, doesn't try for non git repositories.

--- env.sh
+++ env.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 export PYTHONPATH=$PWD:$PWD/core:$PWD/packages:$PYTHONPATH
 echo "snoing setup"
-git pull
-echo "snoing updated."
+printf "%-50s" "Checking for git..."
+which git &> /dev/null
+test $? -eq 0 && printf "Installed\n" || printf "Not Installed\n"
+printf "%-50s" "Checking if this is a git repository..."
+git branch &> /dev/null
+test $? -eq 0 && USEGIT=1 || USEGIT=0
+test $USEGIT -eq 1 && printf "yes\nAttempting update via git pull...\n" || printf "no\n"
+test $USEGIT -eq 1 && git pull

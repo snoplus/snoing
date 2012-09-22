084274b97cf3d7ea977b990b2ab49bdd08a33621
Philip.Jones <p.g.jones@qmul.ac.uk>
2012-09-22 13:36:55 +0100

    Refactored the xerces-c packages.

---
+++ versions/xercescversions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# XercesC311
+#
+# The xercesc release versions
+#
+# Author P G Jones - 19/05/2012 <p.g.jones@qmul.ac.uk> : First revision
+# Author P G Jones - 22/09/2012 <p.g.jones@qmul.ac.uk> : Major refactor of snoing.
+####################################################################################################
+import XercesC
+
+class XercesC311(XercesC.XercesC):
+    """ XercesC 3.1.1 version."""
+    def __init__(self, system):
+        super(XercesC311, self).__init__("xerces-c-3.1.1", system, "xerces-c-3.1.1.tar.gz")

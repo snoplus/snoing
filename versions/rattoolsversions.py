a0740ec8373a3a178e3e83b4ec2768621c697181
Philip.Jones <p.g.jones@qmul.ac.uk>
2013-04-15 15:29:30 +0100

    Add fixed release rat-tools versions 4, 4.1, 4.2

--- versions/rattoolsversions.py
+++ versions/rattoolsversions.py
@@ -13,6 +13,23 @@ class RatToolsDev(rattools.RatToolsDevelopment):
         """ Initialise dev version."""
         super(RatToolsDev, self).__init__("rattools-dev", system, "root-5.34.02")
 
+class RatTools42(rattools.RatToolsRelease):
+    def __init__(self, system):
+        """ Initialise an arbitrary snaphot version."""
+        super(RatTools42, self).__init__("rattools-4.2", system, "root-5.34.02", "rat-4.2",
+                                        "release-4.20")
+
+class RatTools41(rattools.RatToolsRelease):
+    def __init__(self, system):
+        """ Initialise an arbitrary snaphot version."""
+        super(RatTools41, self).__init__("rattools-4.1", system, "root-5.34.02", "rat-4.1",
+                                        "release-4.10")
+class RatTools4(rattools.RatToolsRelease):
+    def __init__(self, system):
+        """ Initialise an arbitrary snaphot version."""
+        super(RatTools4, self).__init__("rattools-4", system, "root-5.32.04", "rat-4",
+                                        "release-4.00")
+
 class RatTools1(rattools.RatToolsRelease):
     def __init__(self, system):
         """ Initialise an arbitrary snaphot version."""

434e877ea82f936f6a304005b2fa6c810dae77a3
Matt Mottram <mjmottram@gmail.com>
2013-10-25 12:22:31 +0100

    add rattools-4.4.0

--- versions/rattoolsversions.py
+++ versions/rattoolsversions.py
@@ -13,6 +13,12 @@ class RatToolsDev(rattools.RatToolsDevelopment):
         """ Initialise dev version."""
         super(RatToolsDev, self).__init__("rattools-dev", system, "root-5.34.08")
 
+class RatTools44(rattools.RatToolsRelease):
+    def __init__(self, system):
+        """ Initialise an arbitrary snaphot version."""
+        super(RatTools44, self).__init__("rattools-4.4.0", system, "root-5.34.08", "rat-4.4.0",
+                                         "release-4.4.0")
+
 class RatTools43Builder423(rattools.RatToolsRelease):
     def __init__(self, system):
         """ Initialise an arbitrary snaphot version."""

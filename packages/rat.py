59357794e423f0c7510370cb13d65eb8286d134f
pgjones <p.g.jones@qmul.ac.uk>
2013-11-12 17:25:59 +0000

    Move to geant4.9.6.p02 and remove xerces-c from rat-dev.

--- packages/rat.py
+++ packages/rat.py
@@ -110,28 +110,21 @@ class RatDevelopment(Rat):
     """ Base rat installer for rat-dev."""
     def __init__(self, name, system):
         """ Initialise rat with the tar_name."""
-        super(RatDevelopment, self).__init__(name, system, "root-5.34.11", "geant4.9.5.p01", "scons-2.1.0")
+        super(RatDevelopment, self).__init__(name, system, "root-5.34.11", "geant4.9.6.p02", "scons-2.1.0")
     def _get_dependencies(self):
         """ Return the extra dependencies."""
-        return ["clhep-2.1.1.0", "curl-7.26.0", "bzip2-1.0.6", "xerces-c-3.1.1" ]
+        return ["curl-7.26.0", "bzip2-1.0.6"]
     def _is_downloaded(self):
         """ Check if tarball has been downloaded."""
         return os.path.exists(self.get_install_path())
     def _download(self):
         """ Git clone rat-dev."""
-        self._system.execute_command("git", ["clone", "git@github.com:snoplus/rat.git",  
-                                             self.get_install_path()], 
+        self._system.execute_command("git", ["clone", "git@github.com:snoplus/rat.git", self.get_install_path()], 
                                      verbose=True)
     def _write_env_file(self):
         """ Write the environment file required for the current rat-dev."""
         self._env_file.add_source(self._dependency_paths[self._geant_dep], "bin/geant4")
-        self._env_file.append_library_path(os.path.join(self._dependency_paths["clhep-2.1.1.0"], "lib"))
-        if self._dependency_paths["xerces-c-3.1.1"] is not None: # Conditional Package
-            self._env_file.add_environment("XERCESCROOT", self._dependency_paths["xerces-c-3.1.1"])
-            self._env_file.append_library_path(os.path.join(self._dependency_paths["xerces-c-3.1.1"], "lib"))
-        self._env_file.append_path(os.path.join(self._dependency_paths["clhep-2.1.1.0"], "bin"))
         self._env_file.append_path(os.path.join(self._dependency_paths[self._geant_dep], "bin"))
-        self._env_file.append_library_path(os.path.join(self._dependency_paths["clhep-2.1.1.0"], "lib"))
         if self._dependency_paths["curl-7.26.0"] is not None: # Conditional Package
             self._env_file.append_path(os.path.join(self._dependency_paths["curl-7.26.0"], "bin"))
             self._env_file.append_library_path(os.path.join(self._dependency_paths["curl-7.26.0"], "lib"))

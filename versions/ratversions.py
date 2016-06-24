61ae70e714d7ac05e307669b0c9d0b78afb1efef
Matthew Parnell <matt@parnmatt.co.uk>
2016-06-24 09:06:57 -0400

    Add RAT 6.1.0 (#138)

--- versions/ratversions.py
+++ versions/ratversions.py
@@ -21,6 +21,12 @@ class RATDev(rat.RatDevelopment):
         """ Initiliase the rat dev package."""
         super(RATDev, self).__init__("rat-dev", system)
 
+class RAT610(ratreleases.RatRelease6):
+    """ Rat release-6.1.0, install package."""
+    def __init__(self, system):
+        """ Initiliase the rat 6.1.0 package."""
+        super(RAT6001, self).__init__("rat-6.1.0", system, "root-5.34.34", "6.1.0")
+
 class RAT601(ratreleases.RatRelease6):
     """ Rat release-6.0.1, install package."""
     def __init__(self, system):

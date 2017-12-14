0c86eda116576ae649351a0f68fa77b833eaa2ee
Matthew Parnell <matt@parnmatt.co.uk>
2017-12-14 15:36:56 +0000

    Add RAT and RAT-tools v6.4.2

--- versions/rattoolsversions.py
+++ versions/rattoolsversions.py
@@ -21,6 +21,12 @@ class RatTools6130(rattools.RatToolsRelease):
         super(RatTools6130, self).__init__("rattools-6.13.0", system, "root-5.34.36", "rat-6.13.0", "6.13.0")
 
 
+class RatTools642(rattools.RatToolsRelease):
+    def __init__(self, system):
+        """ Initialise an arbitrary snaphot version."""
+        super(RatTools642, self).__init__("rattools-6.4.2", system, "root-5.34.36", "rat-6.4.2", "6.4.2")
+
+
 class RatTools641(rattools.RatToolsRelease):
     def __init__(self, system):
         """ Initialise an arbitrary snaphot version."""
--- versions/ratversions.py
+++ versions/ratversions.py
@@ -29,6 +29,13 @@ class RAT6130(ratreleases.RatRelease6):
         super(RAT6130, self).__init__("rat-6.13.0", system, "root-5.34.36", "6.13.0")
 
 
+class RAT642(ratreleases.RatRelease6):
+    """ Rat release-6.4.2, install package."""
+    def __init__(self, system):
+        """ Initiliase the rat 6.4.2 package."""
+        super(RAT642, self).__init__("rat-6.4.2", system, "root-5.34.36", "6.4.2")
+
+
 class RAT641(ratreleases.RatRelease6):
     """ Rat release-6.4.1, install package."""
     def __init__(self, system):

9746b320f20a054dad03214a64e29a0579fd43eb
Matthew Parnell <matt@parnmatt.co.uk>
2018-06-12 12:45:36 +0100

    Use distutils version comparison

--- packages/cmake.py
+++ packages/cmake.py
@@ -8,10 +8,12 @@
 # Author P G Jones - 24/09/2012 <p.g.jones@qmul.ac.uk> : Major refactor of snoing.
 ####################################################################################################
 
-import conditionalpackage
+from conditionalpackage import ConditionalPackage
+from distutils.version import StrictVersion
+import re
 import os
 
-class Cmake(conditionalpackage.ConditionalPackage):
+class Cmake(ConditionalPackage):
     """Cmake install package."""
 
     def __init__(self, name, system, tar_name):
@@ -25,25 +27,12 @@ class Cmake(conditionalpackage.ConditionalPackage):
 
     def _is_system_installed(self):
         """Check if installed on the system, will require at least version 2.8.1."""
-
-        if self._system.find_library('cmake') is not None:  # A version of cmake exists
-            version_string = self._system.execute_command('cmake', ['--version']).split()[2]
-            version_numbers = version_string.split('.')
-
-            # Check cmake 2.8.1 or above is installed
-            if int(version_numbers[0]) < 2:  # No good
-                return False
-            elif (len(version_numbers) == 2
-                    and int(version_numbers[0]) == 2
-                    and int(version_numbers[1][0]) < 8):  # <2.8 bad
-                return False
-            elif (len(version_numbers) == 3
-                    and int(version_numbers[0]) == 2
-                    and int(version_numbers[1]) == 8
-                    and int(version_numbers[2][0]) < 1):
-                return False
-            else:  # All others are good
-                return True
+        if self._system.find_library('cmake') is None:
+            return False
+        version = self._system.execute_command('cmake', ['--version'])
+        version = re.search(r'(\d+\.*)+', version).group()
+        version = StrictVersion(version)
+        return StrictVersion('2.8.1') <= version
 
     def _is_downloaded(self):
         """Check if tar file exists."""

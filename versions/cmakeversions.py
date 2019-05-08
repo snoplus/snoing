7ec61ebecc800acc646d04890184c0ec3d574ec3
kgilje <gilje@ualberta.ca>
2019-05-08 13:20:14 -0600

    Adding an updated version of cmake.  This is a requirement for later Root 6 versions (after root 6.06.08).

--- versions/cmakeversions.py
+++ versions/cmakeversions.py
@@ -8,6 +8,15 @@
 ####################################################################################################
 import cmake
 
+class Cmake3143(cmake.Cmake):
+    """ Cmake 3.14.3, install package."""
+    def __init__(self, system):
+        """ Initiliase the cmake 3.14.3 package."""
+        super(Cmake3143, self).__init__("cmake-3.14.3", system, "cmake-3.14.3.tar.gz")
+    def _download(self):
+        """ Download the 3.14.3 version."""
+        self._system.download_file("http://www.cmake.org/files/v3.14/" + self._tar_name)
+
 class Cmake2812(cmake.Cmake):
     """ Cmake 2.8.12, install package."""
     def __init__(self, system):
